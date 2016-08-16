232f8fa15f6345ec4d7500e1cfeeede33f62b2a4
ENAYA <AENAYA@unrwa.org>
2016-08-17 02:22:54 +0300

    create android jokes lib

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -43,10 +43,10 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
+/*
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-    }
-
+     Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+}
+*/
 
 }
